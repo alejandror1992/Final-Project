32c8ceb27c638a708932f7cfc3beaab8491c46e2
alejandror1992 <arebolledo92@hotmail.com>
2023-05-12 11:52:54 -0500

    "Urls Corrections".

--- AppDjango/urls.py
+++ AppDjango/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from AppDjango import views
 
 urlpatterns = [
-    path('Home/', views.home, name='Home'),
-    path('Academy/', views.academy, name='Academy'),
-    path('Event/', views.events, name='Events'),
-    path('Form/', views.form, name='Form'),
-    path('Academy/edit/<int:pk>/',  views.edit_academy, name='edit_academy'),
+    path('home/', views.home, name='home'),
+    path('academy/', views.academy, name='academy'),
+    path('event/', views.events, name='events'),
+    path('form/', views.form, name='form'),
+    path('academy/edit/<int:pk>/',  views.edit_academy, name='edit_academy'),
     path('profile/edit/', views.edit_user, name='edit_user'),
     path('profile/password/', views.change_password, name='change_password'),
 ]
