a4f9d8149052edb9b1efca64cc4613e09cdce851
alejandror1992 <arebolledo92@hotmail.com>
2023-05-22 18:42:24 -0500

    "Settings".

--- AppUsers/urls.py
+++ AppUsers/urls.py
@@ -1,6 +1,7 @@
+from django.contrib import admin
 from django.urls import path
 from AppUsers.views import register, login_view, CustomLogoutView
 
 path ('register/', register, name='register'),
 path ('login/', login_view, name='login'),
-path ('logout/', CustomLogoutView, name='logout'),
+path ('logout/', CustomLogoutView.as_view(), name='logout'),
