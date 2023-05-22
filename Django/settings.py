d326d88d90f4f1952dd62215a1c045dd1fc38dc3
alejandror1992 <arebolledo92@hotmail.com>
2023-05-22 18:18:46 -0500

    "Settings Changes".

--- Django/settings.py
+++ Django/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from AppUsers.urls import login
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,4 +126,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL:'login'
+LOGIN_URL:'login' 
