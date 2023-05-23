eb75ebaf2be0ad0b353b4cdb006219abf5adf3e4
alejandror1992 <arebolledo92@hotmail.com>
2023-05-23 14:48:27 -0500

    "url changes".

--- Django/urls.py
+++ Django/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('AppDjango/', include('AppDjango.urls')),
-    path('AppUsers/', include('AppUser.urls')),
+    path('AppUsers/', include('AppUsers.urls')),
 ]
 
 
