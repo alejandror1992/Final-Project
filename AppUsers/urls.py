0fb5a7d86590f0f844401fb5ed09c5f0320c01ce
alejandror1992 <arebolledo92@hotmail.com>
2023-05-23 15:01:03 -0500

    "Urls changes".

--- AppUsers/urls.py
+++ AppUsers/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from AppUsers.views import register, login_view, CustomLogoutView
 
+urlpatterns = [ 
 path ('register/', register, name='register'),
 path ('login/', login_view, name='login'),
 path ('logout/', CustomLogoutView.as_view(), name='logout'),
+ ]
