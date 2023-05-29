675dfcf22ca453bc8b2fbcbfc63a6432a15baed0
alejandror1992 <arebolledo92@hotmail.com>
2023-05-29 10:02:52 -0500

    "forms changes".

--- AppDjango/form.py
+++ AppDjango/form.py
@@ -7,15 +7,43 @@ class EventForm(forms.ModelForm):
         fields = ['name', 'location', 'date', 'description']
 
 class AcademyForm(forms.ModelForm):
-    def __init__(self,*args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['styles'].queryset = Style.allowed_objects.all()
+    styles = forms.ModelMultipleChoiceField(queryset=Style.allowed_objects.all(), widget=forms.CheckboxSelectMultiple)
+
     class Meta:
         model = Academy
-        fields = ['name', 'address', 'description', 'styles', 'created_by', 'featured']
+        fields = ['name', 'address', 'description', 'created_by', 'featured']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.fields['styles'].initial = self.instance.styles.values_list('pk', flat=True)
+
+    def save(self, commit=True):
+        academy = super().save(commit=False)
+        if commit:
+            academy.save()
+        if academy.pk:
+            academy.styles.set(self.cleaned_data['styles'])
+            self.save_m2m()
+        return academy
 
 class ProfileForm(forms.ModelForm):
-    styles = forms.ModelMultipleChoiceField(queryset= Style.objects.all())
+    styles = forms.ModelMultipleChoiceField(queryset=Style.allowed_objects.all(), widget=forms.CheckboxSelectMultiple)
+
     class Meta:
         model = UserProfile
-        fields = ['user','bio', 'medals', 'amateur_record', 'professional_record', 'styles', 'academies_visited']
+        fields = ['user', 'bio', 'medals', 'amateur_record', 'professional_record', 'styles', 'academies_visited']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.fields['styles'].initial = self.instance.styles.values_list('pk', flat=True)
+
+    def save(self, commit=True):
+        profile = super().save(commit=False)
+        if commit:
+            profile.save()
+        if profile.pk:
+            profile.styles.set(self.cleaned_data['styles'])
+            self.save_m2m()
+        return profile
